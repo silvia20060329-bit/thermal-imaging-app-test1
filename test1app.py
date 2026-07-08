4b83614caf8373c06c9e296d1aa75228b3834fdb
chingsunny <166902706+chingsunny@users.noreply.github.com>
2026-07-08 15:27:39 +0800

    Update test1app.py

--- test1app.py
+++ test1app.py
@@ -40,7 +40,7 @@ with st.sidebar:
 
     st.subheader("高溫偵測")
     high_threshold = st.slider("高溫亮度門檻", 0, 255, 200, 1)
-    high_min_area = st.slider("高溫最小面積", 0, 5000, 50, 10)
+    high_min_area = st.slider("高溫最小面積", 0, 5000, 50, 50)
 
     st.subheader("低溫偵測")
     cold_quantile = st.slider("低溫候選比例", 0.05, 0.50, 0.20, 0.05)
