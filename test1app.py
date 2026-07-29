83cd53498be7775995cad9b6482732e2de6070ae
chingsunny <166902706+chingsunny@users.noreply.github.com>
2026-07-29 18:27:42 +0800

    Update test1app.py

--- test1app.py
+++ test1app.py
@@ -948,18 +948,17 @@ if st.button("🚀 開始分析", type="primary", use_container_width=True):
 * 🔥 **Wall 高溫**：{results['wall_high_ratio']}%
 * ❄️ **Wall 低溫**：{results['wall_cold_ratio']}%
 
+* **U-Net 材質分布比例（佔整張圖）：**
+
+** 紅磚(material_1)**：{results['material_ratios']['紅磚']}%
+** 短磚(floor_2)   **：{results['material_ratios']['短磚']}%
+** 水泥(material_3)**：{results['material_ratios']['水泥']}%
+** 瓷磚(floor_4)   **：{results['material_ratios']['瓷磚']}%
+** 石磚(wall_2)    **：{results['material_ratios']['石磚']}%
+
 **其他資訊：**
 
 * **YOLO 牆壁/地板目標區域比例**：{results['target_ratio']}%
-
-* **U-Net 材質分布比例（佔整張圖）：**
-
-**紅磚(material_1)**：{results['material_ratios']['紅磚']}%
-**短磚(floor_2)   **：{results['material_ratios']['短磚']}%
-**水泥(material_3)**：{results['material_ratios']['水泥']}%
-**瓷磚(floor_4)   **：{results['material_ratios']['瓷磚']}%
-**石磚(wall_2)    **：{results['material_ratios']['石磚']}%
-
 * **全圖高溫候選比例**：{results['high_ratio_full']}%
 * **全圖低溫候選比例**：{results['cold_ratio_full']}%
 
